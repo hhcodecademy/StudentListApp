4952b271aac1bbc8a05468a5a91d4bab49d93c51
Huseyn Hasanli <huseyn.hasanli@code.edu.az>
2024-01-27 20:05:28 +0400

    Add project files.

---
+++ StudentListApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+
+namespace StudentListApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char answer = 'N';
+            StringBuilder studentList = new StringBuilder();
+            do
+            {
+                Console.WriteLine("Telebin adini daxil edin");
+                string name = Console.ReadLine();
+                Console.WriteLine("Telebin qiymetini daxil edin");
+                float point = float.Parse(Console.ReadLine());
+                Console.WriteLine("Yeni telebe varmi (y/n)");
+                answer = char.Parse(Console.ReadLine());
+
+                string studenInfo = $"{name} - {point}";
+                studentList.AppendLine(studenInfo);
+            } while (answer=='y');
+
+            Console.WriteLine("Telebelerin siyahisi:");
+            Console.WriteLine(studentList);
+        }
+    }
+}
